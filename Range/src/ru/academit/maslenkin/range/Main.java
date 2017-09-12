72df8cb8c92ed6976d3a102e32884b9af39260a1
Aleks <karibasy@bk.ru>
2017-09-12 18:42:44 +0700

    first commit

---
+++ Range/src/ru/academit/maslenkin/range/Main.java
@@ -0,0 +1,21 @@
+package ru.academit.maslenkin.range;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите начало диапазона");
+        double from = scanner.nextDouble();
+
+        System.out.println("Введите конец диапазона");
+        double to = scanner.nextDouble();
+
+        System.out.println("Введите число");
+        double x = scanner.nextDouble();
+
+        Range range = new Range(from, to);
+        boolean result = range.isInside(x);
+        System.out.println(result);
+    }
+}
---
+++ Range/src/ru/academit/maslenkin/range/Range.java
@@ -0,0 +1,31 @@
+package ru.academit.maslenkin.range;
+
+public class Range {
+    private double from;
+    private double to;
+
+    Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    boolean isInside(double x) {
+        return x >= from && x <= to;
+    }
+}
