eedbd525986d68b0f47547932d705180078cda54
Aleks <karibasy@bk.ru>
2018-03-12 18:44:39 +0700

    Minesweeper

--- Minesweeper/src/ru/academit/maslenkin/Main.java
+++ Minesweeper/src/ru/academit/maslenkin/Main.java
@@ -1,10 +1,13 @@
 package ru.academit.maslenkin;
 
+import ru.academit.maslenkin.gui.Frame;
+import ru.academit.maslenkin.view.View;
+
 /**
  * Created by Александр on 12.02.2018.
  */
 public class Main {
     public static void main(String[] args) {
-
+        View frame = new Frame();
     }
 }
--- Minesweeper/src/ru/academit/maslenkin/gui/Frame.java
+++ Minesweeper/src/ru/academit/maslenkin/gui/Frame.java
@@ -1,32 +1,61 @@
 package ru.academit.maslenkin.gui;
 
+import ru.academit.maslenkin.view.View;
+import ru.academit.maslenkin.view.ViewListener;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 /**
  * Created by Александр on 10.03.2018.
  */
-public class Frame extends JFrame {
+public class Frame implements View {
+    private final ArrayList<ViewListener> listeners = new ArrayList<>();
+    private final JFrame frame = new JFrame();
 
-    public Frame() {
+    /*public Frame() {
 
         initFrame();
-    }
+    }*/
 
     private void initFrame() {
-        pack();
-        setTitle("Minesweeper");
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setResizable(false);
-        setVisible(true);
+        frame.pack();
+        frame.setTitle("Minesweeper");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
 
     }
 
     private Image getImage(String name) {
-        String filename = "image/" + name + ".png";
+        String filename = "images/" + name + ".png";
         ImageIcon imageIcon = new ImageIcon(getClass().getResource(filename));
         return imageIcon.getImage();
     }
 
+    @Override
+    public void startApplication() {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                initFrame();
+
+            }
+        });
+
+    }
+
+    @Override
+    public void addViewListener(ViewListener listener) {
+        if (!listeners.contains(listener)) {
+            listeners.add(listener);
+        }
+    }
+
+    @Override
+    public void removeViewListener(ViewListener listener) {
+
+    }
 }
